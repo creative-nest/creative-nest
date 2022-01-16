7699b2aadda191f633b8639a1da0c2ba4c90ca8c
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-01-16 14:14:57 +0100

    Add images to markup

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,27 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import logo from '../public/creativenest_birdLogo_nobg.svg'
+
+import logo from '../public/bird-flat.svg'
+
+import cn_artwork from '../public/birb-sticker.png'
+
+import pb_andreas from '../public/pb-andreas.jpeg'
+import pb_christian from '../public/pb-christian.jpeg'
+import pb_erik from '../public/pb-erik.png'
+import pb_frederik from '../public/pb-frederik.png'
+import pb_jonathan from '../public/pb-jonathan.jpeg'
+import pb_julie from '../public/pb-julie.jpeg'
+import pb_kristoffer from '../public/pb-kristoffer.jpeg'
+import pb_nicklas from '../public/pb-nicklas.jpeg'
+import pb_nicolas from '../public/pb-nicolas.jpeg'
+import pb_rebecca from '../public/pb-rebecca.jpeg'
+
+import logo_mb from '../public/logo-mb.svg'
+import logo_fk from '../public/logo-fk.svg'
+import logo_tec from '../public/logo-tec.svg'
+import logo_nb from '../public/logo-nb.svg'
+import logo_lh from '../public/logo-lh.svg'
 
 export default function Home() {
   return (
@@ -32,8 +52,9 @@ export default function Home() {
           <Link href="/contact">Describe your idea</Link>
           <Link href="/contact">Contact us</Link>
         </div>
-        <div className={styles.mainRight}>
-          <img alt="Creative Nest artwork" />
+        <div className={styles.gradient}></div>
+        <div className={styles.mainArtContainer}>
+          <Image className={styles.mainArt} src={cn_artwork} alt="Creative Nest artwork" />
         </div>
       </main>
       <section className={styles.gallery}>
@@ -44,50 +65,57 @@ export default function Home() {
           <p className={styles.description}>Projects that have taken flight.</p>
         </div>
         <div className={styles.galleryRight}>
-          <div className={styles.carrouselItem}>Facade Design</div>
+          <div className={styles.carrouselItem}>
+            <img alt="Modus Management website" />
+            <p>Website</p>
+          </div>
         </div>
       </section>
       <section>
         <h1 className={styles.title}>
           Customers
         </h1>
-        <img alt="Maltbazaren" />
-        <img alt="Furesø Kommune" />
-        <img alt="TEC" />
-        <img alt="Niels Brock" />
-        <img alt="Ledernes Hovedorganisation" />
+        <div className={styles.customerLogoContainer}>
+          <Image className={styles.customerLogo} src={logo_mb} alt="Maltbazaren" />
+          <Image className={styles.customerLogo} src={logo_fk} alt="Furesø Kommune" />
+          <Image className={styles.customerLogo} src={logo_tec} alt="TEC" />
+          <Image className={styles.customerLogo} src={logo_nb} alt="Niels Brock" />
+          <Image className={styles.customerLogo} src={logo_lh} alt="Ledernes Hovedorganisation" />
+        </div>
       </section>
       <section>
         <h1 className={styles.title}>
           Trust pilot testimonials
         </h1>
         <div>
-          <img alt="Klaus Langbergs" />
+          <h3>Klaus Langbergs</h3>
           <p>Fantastisk service og et resultat i sidste ende, der virkeligt er fedt! Vi fik lavet et helt nyt design til vores glasfacade I min virksomhed.Unge og professionelle mennesker, der brænder for hvad de laver. Kan varmt anbefales!! </p>
         </div>
         <div>
-          <img alt="Herlev Kebab og Pizza" />
+          <h3>Gokhan Bilen</h3>
           <p>En klar anbefaling. God indledende dialog om opgaven og super godt udført arbejde. Fotografen evnede virkelig, at fange stemningerne ind i billederne.</p>
         </div>
       </section>
       <section>
         <h1>About The Nest</h1>
         <p>Creative Nest is community and a mix of passionate creative talents, with a goal of developing our skills, to deliver kick-ass creative media product to the world.</p>
-        <img alt="jonathan strange" />
-        <img alt="nicklas casthøj" />
-        <img alt="rebecca strange" />
-        <img alt="erik hertel" />
-        <img alt="frederik væversted" />
-        <img alt="julie kølle" />
-        <img alt="mathias andersen" />
-        <img alt="andreas holm" />
-        <img alt="nicolas brandt haurum" />
-        <img alt="christian valerius" />
-        <img alt="kristoffer højelse" />
+        <div className={styles.peopleContainer}>
+          <Image src={pb_jonathan} alt="jonathan strange" />
+          <Image src={pb_nicklas} alt="nicklas casthøj" />
+          <Image src={pb_rebecca} alt="rebecca strange" />
+          <Image src={pb_erik} alt="erik hertel" />
+          <Image src={pb_frederik} alt="frederik væversted" />
+          <Image src={pb_julie} alt="julie kølle" />
+          <img alt="mathias andersen" />
+          <Image src={pb_andreas} alt="andreas holm" />
+          <Image src={pb_nicolas} alt="nicolas brandt haurum" />
+          <Image src={pb_christian} alt="christian valerius" />
+          <Image src={pb_kristoffer} alt="kristoffer højelse" />
+        </div>
       </section>
       <footer>
         <div>
-          <img alt="Create Nest logo" />
+          <Image className={styles.logo} height="50px" width="50px" src={logo} alt="Creative Nest logo" />
           <h1>Creative Nest</h1>
           <p>hello@creativenest.dk</p>
           <p>+45 88 88 88 88</p>
