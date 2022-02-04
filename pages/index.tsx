623a34c7030c516ee185d154c3d7184f4046292c
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-02-04 18:47:58 +0100

    Make styles global

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
-import logo from '../public/bird-flat.svg'
+import logo_cn from '../public/bird-flat.svg'
 
 import cn_artwork from '../public/birb-sticker.png'
 
@@ -26,15 +25,15 @@ import logo_lh from '../public/logo-lh.svg'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>Creative Nest</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav className={styles.nav}>
+      <nav className="nav">
         <div>
           <Link href="/">
-            <Image className={styles.logo} height="50px" width="50px" src={logo} alt="Creative Nest logo" />
+            <Image className="logo" height="50px" width="50px" src={logo_cn} alt="Creative Nest logo" />
           </Link>
         </div>
         <div>
@@ -43,48 +42,48 @@ export default function Home() {
           <Link href="/contact">Describe your idea</Link>
         </div>
       </nav>
-      <main className={styles.main}>
-        <div className={styles.mainLeft}>
-          <h1 className={styles.title}>
+      <main className="main">
+        <div className="main-left">
+          <h1 className="title">
             Creative Nest
           </h1>
-          <p className={styles.description}>Hatching your creative ideas into to fully grown realities. We develop projects across foto, video, audio and design.</p>
+          <p className="description">Hatching your creative ideas into to fully grown realities. We develop projects across foto, video, audio and design.</p>
           <Link href="/contact">Describe your idea</Link>
           <Link href="/contact">Contact us</Link>
         </div>
-        <div className={styles.gradient}></div>
-        <div className={styles.mainArtContainer}>
-          <Image className={styles.mainArt} src={cn_artwork} alt="Creative Nest artwork" />
+        <div className="gradient"></div>
+        <div className="main-art-container">
+          <Image className="main-art" src={cn_artwork} alt="Creative Nest artwork" />
         </div>
       </main>
-      <section className={styles.gallery}>
-        <div className={styles.galleryLeft}>
-          <h1 className={styles.title}>
+      <section className="gallery">
+        <div className="gallery-left">
+          <h1 className="title">
             Gallery
           </h1>
-          <p className={styles.description}>Projects that have taken flight.</p>
+          <p className="description">Projects that have taken flight.</p>
         </div>
-        <div className={styles.galleryRight}>
-          <div className={styles.carrouselItem}>
-            <img alt="Modus Management website" />
+        <div className="gallery-right">
+          <div className="carrousel-item">
+            <Image layout='fill' src={`https://via.placeholder.com/800.webp/`} alt="Modus Management website" />
             <p>Website</p>
           </div>
         </div>
       </section>
       <section>
-        <h1 className={styles.title}>
+        <h1 className="title">
           Customers
         </h1>
-        <div className={styles.customerLogoContainer}>
-          <Image className={styles.customerLogo} src={logo_mb} alt="Maltbazaren" />
-          <Image className={styles.customerLogo} src={logo_fk} alt="Furesø Kommune" />
-          <Image className={styles.customerLogo} src={logo_tec} alt="TEC" />
-          <Image className={styles.customerLogo} src={logo_nb} alt="Niels Brock" />
-          <Image className={styles.customerLogo} src={logo_lh} alt="Ledernes Hovedorganisation" />
+        <div className="customer-logo-container">
+          <Image className="customer-logo" src={logo_mb} alt="Maltbazaren" />
+          <Image className="customer-logo" src={logo_fk} alt="Furesø Kommune" />
+          <Image className="customer-logo" src={logo_tec} alt="TEC" />
+          <Image className="customer-logo" src={logo_nb} alt="Niels Brock" />
+          <Image className="customer-logo" src={logo_lh} alt="Ledernes Hovedorganisation" />
         </div>
       </section>
       <section>
-        <h1 className={styles.title}>
+        <h1 className="title">
           Trust pilot testimonials
         </h1>
         <div>
@@ -99,7 +98,7 @@ export default function Home() {
       <section>
         <h1>About The Nest</h1>
         <p>Creative Nest is community and a mix of passionate creative talents, with a goal of developing our skills, to deliver kick-ass creative media product to the world.</p>
-        <div className={styles.peopleContainer}>
+        <div className="people-container">
           <Image src={pb_jonathan} alt="jonathan strange" />
           <Image src={pb_nicklas} alt="nicklas casthøj" />
           <Image src={pb_rebecca} alt="rebecca strange" />
@@ -115,7 +114,7 @@ export default function Home() {
       </section>
       <footer>
         <div>
-          <Image className={styles.logo} height="50px" width="50px" src={logo} alt="Creative Nest logo" />
+          <Image className="logo" height="50px" width="50px" src={logo_cn} alt="Creative Nest logo" />
           <h1>Creative Nest</h1>
           <p>hello@creativenest.dk</p>
           <p>+45 88 88 88 88</p>
